293c95baad39dba07ce8bc8e9dbf9f694dce35d8
Alexandr25041973 <sashan25041973@yandex.ru>
2023-07-25 21:25:10 +0300

    Add change task 038 Seminar5

--- HW_task038/Program.cs
+++ HW_task038/Program.cs
@@ -2,32 +2,35 @@
 Найдите разницу между максимальным и минимальным элементами массива.
 [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76 */
 
-/*double[] RandomArrDouble(int size, double min, double max)//Метод массива веществ чисел
+// 1 вариант решения(мой):
+
+double[] RandomArrDouble(int size, double min, double max)//Метод массива веществ чисел случ
 {
   double[] array = new double[size];
   for (int i = 0; i < array.Length; i++)
   {
-    array[i] = Convert.ToDouble(new Random().Next(min, max));
+    array[i] = min + (new Random().NextDouble()) * max;
   }
   return array;
-} */
+} 
 
-double[] ArrDouble(string stringArray)
+double[] ArrDouble(string stringArray) //метод преобразования строки в массив веществен чисел
 {
-    string[] nums = stringArray.Split(" ");
-    double[] resdouble = new double[nums.Length];
-    for (int i = 0; i < nums.Length; i++)
-    {
-        //res[i] = Convert.ToInt32(nums[i]);
-        resdouble[i] = Convert.ToDouble(nums[i]);
-    }
-    return resdouble;
+  string[] nums = stringArray.Split(" ");
+  double[] resdouble = new double[nums.Length];
+  for (int i = 0; i < nums.Length; i++)
+  {
+    //resdouble[i] = double.Parse(nums[i]);
+    resdouble[i] = Convert.ToDouble(nums[i]);
+  }
+  return resdouble;
 }
 
-double[] MaxMinDiff(double[] arr1)//Метод суммы положит и отриц значений чз нулевой массив 2х элемент
+double[] MaxMinDiff(double[] arr1)//Метод суммы положит и отриц значений чз нулевой массив 
+//3х элементов, где первый - max, второй - min, третий - разница му max и min .
 {
-  double[] arr2 = new double[2];
-  double max = arr2[0]; 
+  double[] arr2 = new double[3];
+  double max = arr2[0];
   double min = arr2[1];
   foreach (double element in arr1)//вместо int м.б. - string, double, int[] и т.д.;
   {
@@ -39,7 +42,6 @@ double[] MaxMinDiff(double[] arr1)//Метод суммы положит и от
 }
 
 
-
 Console.Clear();
 Console.Write("Введите элементы массива через пробел: ");
 string elements = Console.ReadLine();
@@ -49,4 +51,42 @@ double[] arr3 = ArrDouble(elements);
 Console.Write($"[{String.Join(", ", arr3)}]");
 //Console.Write($"[{String.Join(", ", RandomArrDouble(sizeA, minA, maxA))}] => "+
 //$"{arr4[0]} - {arr4[1]} = {(arr4[0] - arr4[1])}");
+
+
+// ***************************
+/* 2 вариант решения:
+Console.Clear();
+
+double[] ArrDouble(int sizeArray)
+{
+  double[] resdouble = new double[sizeArray];
+  Console.WriteLine("Введите элементы массива : ");
+  for (int i = 0; i < resdouble.Length; i++)
+  {
+    resdouble[i] = Convert.ToDouble(Console.ReadLine());
+  }
+  return resdouble;
+}
+
+double MaxMinDiff(double[] array)//Метод суммы положит и отриц значений чз нулевой массив 2х элемент
+{
+  double max = array[0];
+  double min = array[1];
+
+  for (int i = 1; i < array.Length; i++)
+  {
+    if (max < array[i]) max = array[i];
+    if (min > array[i]) min = array[i];
+  }
+
+  return max - min;
+}
+
+Console.Write("Сколько элементов массива вы хотите ввести ? : ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+double[] array = ArrDouble(sizeArray);
+
+Console.WriteLine("Разница составляет : " + MaxMinDiff(array));
+*/
+//*************************** 
 
